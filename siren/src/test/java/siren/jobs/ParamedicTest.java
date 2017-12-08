e7b1099b14edb2036a40cb7f374d197b0ca36c21
viki <viki@bumbi>
2017-12-08 16:28:03 +0100

    Missing Test annotation added

--- siren/src/test/java/siren/jobs/ParamedicTest.java
+++ siren/src/test/java/siren/jobs/ParamedicTest.java
@@ -1,9 +1,11 @@
 package siren.jobs;
 
 import org.junit.Assert;
+import org.junit.Test;
 
 public class ParamedicTest {
 	
+	@Test
 	public void testParamedicNightWorking_ShouldBeTrue() {
 		Paramedic paramedic = new Paramedic();
 		
