9c989495a85d4d873751dab8fcfa252577e88eba
viki <viki@bumbi>
2017-12-11 17:37:11 +0100

    ShiftTest corrected

--- siren/src/main/java/siren/planners/SimplePlanner.java
+++ siren/src/main/java/siren/planners/SimplePlanner.java
@@ -3,6 +3,7 @@ package siren.planners;
 import java.util.List;
 import java.util.Set;
 
+import siren.components.ECarType;
 import siren.components.Plan;
 import siren.components.Shift;
 import siren.components.Shifts;
@@ -15,10 +16,13 @@ public class SimplePlanner implements IPlannerAlgorithm {
 		for (int i = 1; i <= shifts.getDaysInMonth(); i++) {
 			Set<Shift> dailyShifts = shifts.getShiftsForDay(i);
 			for (Shift s : dailyShifts) {
-
+				ECarType carType = s.getCarType();
+				
+				
+				
 			}
 		}
-		return null;
+		return new Plan(null);
 	}
 
 }
--- siren/src/test/java/siren/components/ShiftTest.java
+++ siren/src/test/java/siren/components/ShiftTest.java
@@ -2,43 +2,48 @@ package siren.components;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.mockito.Mockito;
 
 public class ShiftTest {
 
+	private int day = 4;
+	private int idOfshift = 3;
+	private int testFrom = 8;
+	private int testTo = 18;
+	private ECarType carType = ECarType.ESETKOCSI;
+
 	@Test
 	public void testShiftIsNight_ShouldBeFalse() {
-		Shift shift = new Shift(Mockito.anyInt(), Mockito.anyInt(), 8, 18, Mockito.any(ECarType.class));
+		Shift shift = new Shift(day, idOfshift, 8, 18, carType);
 		Assert.assertFalse(shift.isNight());
 	}
 
 	@Test
 	public void testShiftIsNight_ShouldBeTrue() {
-		Shift shift = new Shift(Mockito.anyInt(), Mockito.anyInt(), 18, 6, Mockito.any(ECarType.class));
+		Shift shift = new Shift(day, idOfshift, 18, 6, carType);
 		Assert.assertTrue(shift.isNight());
 	}
 
 	@Test
 	public void testShiftDuration_ShouldBe12Hour() {
-		Shift shift = new Shift(Mockito.anyInt(), Mockito.anyInt(), 18, 6, Mockito.any(ECarType.class));
+		Shift shift = new Shift(day, idOfshift, 18, 6, carType);
 		Assert.assertEquals(12, shift.duration());
 	}
 
 	@Test
 	public void testShiftIsNight_ShouldBe7Hour() {
-		Shift shift = new Shift(Mockito.anyInt(), Mockito.anyInt(), 8, 15, Mockito.any(ECarType.class));
+		Shift shift = new Shift(day, idOfshift, 8, 15, carType);
 		Assert.assertEquals(7, shift.duration());
 	}
 
 	@Test
 	public void testShiftGetDay_ShouldBe15() {
-		Shift shift = new Shift(15, Mockito.anyInt(), Mockito.anyInt(), Mockito.anyInt(), Mockito.any(ECarType.class));
+		Shift shift = new Shift(15, idOfshift, testFrom, testTo, carType);
 		Assert.assertEquals(15, shift.getDay());
 	}
-	
+
 	@Test
 	public void testGetCarType_ShouldBeRohamkocsi() {
-		Shift shift = new Shift(Mockito.anyInt(), Mockito.anyInt(), Mockito.anyInt(), Mockito.anyInt(), ECarType.ROHAMKOCSI);
+		Shift shift = new Shift(day, idOfshift, testFrom, testTo, ECarType.ROHAMKOCSI);
 		Assert.assertEquals(ECarType.ROHAMKOCSI, shift.getCarType());
 	}
 
